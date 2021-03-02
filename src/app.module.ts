82df0e3b5a7a0aac71de5febc91348cd712219e0
majkl zumberi <majklzumberi00@gmail.com>
2021-03-02 21:35:43 +0100

    style(format): :art: better format for app.module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,50 +3,36 @@ import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { Student } from './student/entities/student.entity';
-import { StudentModule } from './student/student.module';
 import { ClassroomModule } from './classroom/classroom.module';
 import { Classroom } from './classroom/entities/classroom.entity';
 import { CourseModule } from './course/course.module';
 import { Course } from './course/entities/course.entity';
-
+import { Student } from './student/entities/student.entity';
+import { StudentModule } from './student/student.module';
 
 @Module({
-    imports: [StudentModule,
-        ConfigModule.forRoot({
+  imports: [
+    StudentModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
 
-            isGlobal: true,
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: process.env.DB_user,
+      password: process.env.DB_password,
+      database: 'architetturasoftware',
+      entities: [Student, Classroom, Course],
+      synchronize: true,
+      keepConnectionAlive: true,
+    }),
+    ClassroomModule,
+    CourseModule,
+  ],
 
-        }),
-
-        TypeOrmModule.forRoot({
-
-            type: 'mysql',
-
-            host: 'localhost',
-
-            port: 3306,
-
-            username: process.env.DB_user,
-
-            password: process.env.DB_password,
-
-            database: 'architetturasoftware',
-
-            entities: [Student,Classroom,Course],
-
-            synchronize: true,
-
-            keepConnectionAlive: true,
-
-        }),
-
-        ClassroomModule,
-
-        CourseModule,],
-
-    controllers: [AppController],
-    providers: [AppService],
-
+  controllers: [AppController],
+  providers: [AppService],
 })
-export class AppModule { }
+export class AppModule {}
